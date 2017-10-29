6ba29d68d503ad228c18a574a6d61f79ef48bb01
ERElli <EricRoy.Elli@gmail.com>
2017-10-29 18:27:38 -0230

    New class for adding times to the bookable times for rooms

---
+++ src/Schedule/DayOfWeekTimeSpan.java
@@ -0,0 +1,30 @@
+package Schedule;
+
+import java.time.*;
+public class DayOfWeekTimeSpan {
+	private DayOfWeek dayOfWeek;
+	private TimeSpan timeSpan;
+	
+	public DayOfWeekTimeSpan (DayOfWeek day, TimeSpan timeSpan) {
+		this.dayOfWeek = day;
+		this.timeSpan = timeSpan;
+	}
+	
+	public DayOfWeek getDayOfWeek() {
+		return dayOfWeek;
+	}
+	
+	public void setDayOfWeek(DayOfWeek day) {
+		this.dayOfWeek = day;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(dayOfWeek.toString());
+		sb.append("\n");
+		sb.append(timeSpan.toString());
+		return sb.toString();
+	}
+	
+}
