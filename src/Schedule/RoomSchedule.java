5d9123cd8ea6891702c7545c4807d1fb1ce3463a
ERElli <EricRoy.Elli@gmail.com>
2017-10-30 19:02:35 -0230

    Updated to use DayOfWeekTimeSpan for bookable times

--- src/Schedule/RoomSchedule.java
+++ src/Schedule/RoomSchedule.java
@@ -1,6 +1,7 @@
 package Schedule;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import Request.Request;
 import Room.Room;
@@ -8,18 +9,32 @@ import Room.Room;
 public class RoomSchedule {
 	private Room room;
 	private ArrayList<Request> bookings;
-	private ArrayList<TimeSpan> bookableTimes;
+	private ArrayList<DayOfWeekTimeSpan> bookableTimes;
 	
 	public RoomSchedule(Room room) {
 		this.room = room;
-		this.bookableTimes = new ArrayList<TimeSpan>();
+		this.bookableTimes = new ArrayList<DayOfWeekTimeSpan>();
 		this.bookings = new ArrayList<Request>();
 	}
 	
-	public void addBookableTime(TimeSpan span) {
+	public void addBookableTime(DayOfWeekTimeSpan span) {
 		bookableTimes.add(span);
 	}
 	
+	public String getBookableTimesMessage() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("Room ").append(room.getRoomNumber());
+		sb.append(" is bookable from: \n");
+		for (Iterator<DayOfWeekTimeSpan> i = bookableTimes.iterator(); i.hasNext();) {
+			DayOfWeekTimeSpan timeSpan = i.next();
+			sb.append(timeSpan.toString());
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
+		
 	public Room getRoom() {
 		return room;
 	}
@@ -31,4 +46,17 @@ public class RoomSchedule {
 	public void addBookings(Request r) {
 		bookings.add(r);
 	}
+	
+//	@Override 
+//	public String toString() {
+//		StringBuilder sb = new StringBuilder();
+//		
+//		if(!.isEmpty()) {
+//			sb.append(room.getRoomNumber());
+//			sb.append(" is booked for ");
+//			for(Iterator<TimeSpan> i = bookable)
+//		}
+//		
+//		return sb.toString();
+//	}
 }
