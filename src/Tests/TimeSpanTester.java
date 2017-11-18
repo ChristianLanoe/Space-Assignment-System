813a66690af934c6c0e4a51ada6eb0249da7b7b4
ERElli <EricRoy.Elli@gmail.com>
2017-11-18 19:22:49 -0330

    TimeSpan Tester

--- src/Tests/TimeSpanTester.java
+++ src/Tests/TimeSpanTester.java
@@ -1,27 +1,21 @@
 package Tests;
 
+import java.time.LocalTime;
+
 import Schedule.TimeSpan;
 
-import java.util.Calendar;
-
 public class TimeSpanTester {
-	
+	private static int MINUTES = 0;
 	public static void main(String[] args) {
 		
-		Calendar startTime = Calendar.getInstance();
-		Calendar endTime = Calendar.getInstance();
+		LocalTime startTime = LocalTime.of(0, MINUTES);
+		LocalTime endTime = LocalTime.of(23, MINUTES);
 		
-		startTime.set(Calendar.HOUR, 9);
-		startTime.set(Calendar.MINUTE, 0);
-		startTime.set(Calendar.AM_PM, Calendar.AM);
-		startTime.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-		
-		endTime.set(Calendar.HOUR, 10);
-		endTime.set(Calendar.MINUTE,0);
-		endTime.set(Calendar.AM_PM, Calendar.AM);
-		endTime.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-		
-		TimeSpan newTime = new TimeSpan(startTime,endTime);
-		System.out.println(newTime.toString());	
+		TimeSpan ts = new TimeSpan(startTime, endTime);
+		System.out.println(ts.toString());
+		System.out.print("Start Time: ");
+		System.out.println(ts.getStartTime());
+		System.out.print("End Time: ");
+		System.out.println(ts.getEndTime());
 	}
 }
