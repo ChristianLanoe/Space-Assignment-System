e1053ef1afe07b740c4650278c7c266e7f09f8f6
ERElli <EricRoy.Elli@gmail.com>
2017-11-22 09:30:23 -0330

    added forDate function

--- src/Schedule/Calendar.java
+++ src/Schedule/Calendar.java
@@ -1,33 +1,69 @@
 package Schedule;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
-
-import Room.Room;
 
 //Class for Handling all the room schedules
 public class Calendar {
 	private ArrayList<RoomSchedule> calendar;
-	
+
 	public Calendar() {
 		calendar = new ArrayList<RoomSchedule>();
 	}
-	
+
 	public void addRoomSchedule(RoomSchedule rs) {
 		calendar.add(rs);
+		Collections.sort(calendar);
 	}
-	
+
 	public void removeRoomSchedule(RoomSchedule rs) {
 		calendar.remove(rs);
 	}
 	
-	public RoomSchedule getRoomSchedule(Room room) {
+	
+	public Calendar forDate(LocalDate date) {
+		Calendar dateCalendar = new Calendar();
 		for(Iterator<RoomSchedule> i = calendar.iterator(); i.hasNext();) {
 			RoomSchedule rs = i.next();
-			if (rs.getRoom() == room) {
+			RoomSchedule dateSchedule = rs.forDate(date);
+			if(!dateSchedule.getBookableTimes().isEmpty()) {
+				dateCalendar.addRoomSchedule(dateSchedule);			
+			}
+		}
+		return dateCalendar;
+	}
+
+	//returns a string of all the rooms, their bookable times and their approved requests
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (Iterator<RoomSchedule> i = calendar.iterator(); i.hasNext();) {
+			RoomSchedule rs = i.next();
+			sb.append(rs.toString() + "\n");
+		}
+		return sb.toString();
+	}
+
+	//returns the RoomSchedule of a specific Room
+	public RoomSchedule getRoomSchedule(int roomNumber) {
+		for (Iterator<RoomSchedule> i = calendar.iterator(); i.hasNext();) {
+			RoomSchedule rs = i.next();
+			if (rs.getRoom().getRoomNumber() == roomNumber) {
 				return rs;
 			}
 		}
 		return null;
+	}
+	
+	//returns a String of the bookable times and approved requests for a specific room
+	public String getRoomScheduleString(int roomNumber) {
+		for (Iterator<RoomSchedule> i = calendar.iterator(); i.hasNext();) {
+			RoomSchedule rs = i.next();
+			if (rs.getRoom().getRoomNumber() == roomNumber) {
+				return rs.toString();
+			}
+		}
+		return "";
 	}
 }
