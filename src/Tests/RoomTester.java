b7bb4d1d60a47d743769571ea3e9494c3f95807d
ERElli <EricRoy.Elli@gmail.com>
2017-11-24 10:44:14 -0330

    Formatting changes
    removed roomType and amenity

--- src/Tests/RoomTester.java
+++ src/Tests/RoomTester.java
@@ -1,32 +1,15 @@
 package Tests;
 
-import java.util.ArrayList;
-
-import Room.Amenity;
 import Room.Room;
-import Room.RoomType;
 
 public class RoomTester {
 	public static void main(String[] args) {
-		ArrayList<Amenity> a = new ArrayList<>();
-		a.add(Amenity.Chalkboard);
-		a.add(Amenity.DocumentCamera);
-		a.add(Amenity.TV);
-		
 		int CAPACITY = 50;
 		int ROOM_NUMBER = 3001;
-		RoomType ROOM_TYPE = RoomType.Classroom;
 		int LENGTH = 50;
 		int WIDTH = 50;
 		
-		Room room = new Room(ROOM_NUMBER,ROOM_TYPE,CAPACITY,a,LENGTH,WIDTH);
+		Room room = new Room(ROOM_NUMBER,CAPACITY,LENGTH,WIDTH);
 		System.out.println(room.toString());
-		
-		room.addAmenity(Amenity.OverheadProjector);
-		System.out.println("\nAdding an overhead projector");
-		System.out.println(room.toString());
-		
-		System.out.println(room.getAmenities().toString());
-
 	}
 }
