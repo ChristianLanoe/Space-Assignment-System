d2abedfd14822c7d734bd95b8205b87d27a4f8b7
ERElli <EricRoy.Elli@gmail.com>
2017-11-27 09:12:21 -0330

    Removed unused import

--- src/UI/CalendarPanel.java
+++ src/UI/CalendarPanel.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
