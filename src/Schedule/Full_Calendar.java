83e50b1c71c8628bf38f11cfaa3d861806848386
ERElli <32199063+ERElli@users.noreply.github.com>
2017-12-01 09:30:08 -0330

    added comments

--- src/Schedule/Full_Calendar.java
+++ src/Schedule/Full_Calendar.java
@@ -14,6 +14,7 @@ public class Full_Calendar implements Serializable {
 	private int daysInWeek = 7;
 	ArrayList<Full_RoomSchedule> schedules = new ArrayList<>();
 
+	// null constructor
 	public Full_Calendar() {
 		schedules = new ArrayList<Full_RoomSchedule>();
 	}
@@ -32,51 +33,53 @@ public class Full_Calendar implements Serializable {
 		return this.schedules;
 	}
 
-	//Gets the Semester_RoomSchedule 
+	// Gets the Semester_RoomSchedule
 	public Semester_RoomSchedule forSemester(String room, SemesterType sType) {
 		Semester_RoomSchedule result = new Semester_RoomSchedule(room, sType);
-		
+
+		// Iterate through all Full_RoomSchedules
 		for (Full_RoomSchedule fullRS : schedules) {
 			if (fullRS.getRoom().equals(room)) {
 				result = fullRS.forSemester(sType);
 			}
 		}
-		
+
 		return result;
 	}
 
-	public Boolean[][] getAvailableTimes(String room, SemesterType sType){
+	// Creates a 2D Boolean array each row is a day of the week and each column is
+	// the hour block
+	public Boolean[][] getAvailableTimes(String room, SemesterType sType) {
 		Boolean[][] result = new Boolean[daysInWeek][hoursInDay];
-		for(int i = 0; i<daysInWeek; i++) {
-			for(Full_RoomSchedule full_rs : schedules) {
-				if(full_rs.getRoom() == room) {
-					result[i] = full_rs.getAvailable(DayOfWeek.of(i+1), sType);
-				}	
-				//schedules is sorted by room
-				//if we come to a room that is after 'room' lexicographically, we can stop iterating
-				if(full_rs.getRoom().compareTo(room) == 1) {
+		for (int i = 0; i < daysInWeek; i++) {
+			for (Full_RoomSchedule full_rs : schedules) {
+				if (full_rs.getRoom() == room) {
+					result[i] = full_rs.getAvailable(DayOfWeek.of(i + 1), sType);
+				}
+				// schedules is sorted by room
+				// if we come to a room that is after 'room' lexicographically, we can stop
+				// iterating
+				if (full_rs.getRoom().compareTo(room) == 1) {
 					break;
 				}
 			}
 		}
 		return result;
 	}
-	
+
+	// Serializing the Full_Calendar
 	public void serialize() {
 		try {
 			FileOutputStream fos = new FileOutputStream("docs/fullSchedule.ser");
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(this);
-//			fos = new FileOutputStream("docs/Available.ser");
-//			oos = new ObjectOutputStream(fos);
-//			oos.writeObject(this.getAvailableTimes(room, sType));
 			oos.close();
 			fos.close();
-		} catch(IOException i) {
+		} catch (IOException i) {
 			i.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
