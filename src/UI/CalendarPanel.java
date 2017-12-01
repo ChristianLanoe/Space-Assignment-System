9261f29fa1ba5cd38210cce2cc4d69434b0b9048
ERElli <32199063+ERElli@users.noreply.github.com>
2017-12-01 03:21:26 -0330

    Serializable and new schedule architecture
    CalendarPanel now deserializes an object and works with new RoomScheule architecture (Semester_RoomSchedule,Full_RoomSchedule,Full_Calendar)

--- src/UI/CalendarPanel.java
+++ src/UI/CalendarPanel.java
@@ -2,84 +2,67 @@ package UI;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JViewport;
-import javax.swing.table.DefaultTableColumnModel;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
 
-import Request.Request;
-import Room.Room;
-import Room.RoomDB;
-import Schedule.Calendar;
 import Schedule.DayOfWeekTimeSpan;
-import Schedule.RoomSchedule;
+import Schedule.Full_Calendar;
+import Schedule.Full_RoomSchedule;
+import Schedule.SemesterType;
+import Schedule.Semester_RoomSchedule;
 import Schedule.TimeSpan;
 
 // Class for creating panel that displays the calendar for all rooms
-public class CalendarPanel {
+public class CalendarPanel implements Serializable {
 	private JPanel panel;
-	private Calendar cal;
 	private JTable table;
-	private int HoursInDay = 24;
-	public CalendarPanel() {
+	private int hoursInDay = 24;
+	private int daysInWeek = 7;
+	private Full_Calendar fullCalendar;
+	
+	public CalendarPanel(String room, SemesterType type) {	
 		panel = new JPanel();
-
-		initializeRoomSchedules();	
-		Boolean[][] data = cal.getDateArray(cal.forDate(LocalDate.now()));
 		
-		//An array of all the rooms available for a certain day
-		//TODO: get forDate argument from JDatePicker
-		Integer[] rooms = cal.getRoomArray(cal.forDate(LocalDate.now()));
-		Integer[][] roomHeaderValues = new Integer[rooms.length][1];
-		for(int i = 0; i<rooms.length; i++) {
-			roomHeaderValues[i][0] = rooms[i];
+		initializeCalendar();
+		Boolean[][] available = new Boolean[daysInWeek][hoursInDay];
+		for (int i = 0; i< daysInWeek;i++) {
+			available[i] = fullCalendar.forSemester(room,type).availableTimes(DayOfWeek.of(i+1));			
 		}
 		
-		String[] rowHeader = {"Rooms"};
 		
-		
+		String[][] RowHeaderValues = new String[daysInWeek][1];
+		for(int i=0; i<daysInWeek; i++) {
+			RowHeaderValues[i][0] = DayOfWeek.of(i+1).toString();
+		}
+				
 		//An array of all column names (i.e. hour blocks)
-		//TODO: column names in the form "9-10"
-		Integer[] columnNames = new Integer[HoursInDay];
-		for(int i = 0; i<HoursInDay;i++) {
+		Integer[] columnNames = new Integer[hoursInDay];
+		for(int i = 0; i<hoursInDay;i++) {
 			columnNames[i] = i;
 		}
-		
-		DefaultTableModel rowHeaderTableModel = new DefaultTableModel(0,1);
-		for(int i = 0; i<rooms.length;i++) {
-			rowHeaderTableModel.addRow(new Object[] {rooms[i]});			
-		}
-		
-		
-		TableColumnModel rowHeaderModel = new DefaultTableColumnModel() {
-			boolean first= true;
-			
-			public void addColumn(TableColumn tc) {
-				if(first) {
-					tc.setMaxWidth(tc.getPreferredWidth());
-					super.addColumn(tc);
-					first = false;
-				}
-			}
-		};
 
-		JTable rowHeaderColumn = new JTable(roomHeaderValues,rowHeader);
+		String[] rowHeader = {"Days"};
+		JTable rowHeaderColumn = new JTable(RowHeaderValues,rowHeader);
 		rowHeaderColumn.setRowHeight(30);
 		
-		table = new JTable(data,columnNames) {
+		table = new JTable(available,columnNames){
 			@Override
 			public Component prepareRenderer(TableCellRenderer renderer, int rowIndex, int columnIndex) {
-				Component component = super.prepareRenderer(renderer,rowIndex,columnIndex);
+				Component component = (JComponent) super.prepareRenderer(renderer,rowIndex,columnIndex);
 				if(getValueAt(rowIndex,columnIndex).equals(false)) {
 					component.setForeground(Color.RED);
 					component.setBackground(Color.RED);
@@ -103,10 +86,10 @@ public class CalendarPanel {
 		JScrollPane tableScroll = new JScrollPane(table,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		tableScroll.setRowHeaderView(viewport);
 		
-//		tableScroll.setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, rowHeaderColumn.getTableHeader());
 		panel.add(tableScroll);
 	}
 	
+	//Table Preferences
 	public void setTablePreferences() {
 		table.getTableHeader().setReorderingAllowed(false);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
@@ -115,6 +98,8 @@ public class CalendarPanel {
 		table.setShowVerticalLines(true);
 		setJTableColumnsWidth(table,30);
 	}
+	
+	//Setting JTable column widths
 	public static void setJTableColumnsWidth(JTable table, int tablePreferredWidth) {
 	    for (int i = 0; i < table.getColumnModel().getColumnCount(); i++) {
 	        TableColumn column = table.getColumnModel().getColumn(i);
@@ -122,182 +107,24 @@ public class CalendarPanel {
 	    }
 	}
 	
-	private void initializeRoomSchedules() {
-		int MINUTE = 0;
-		
-		//Creating Rooms
-		Room a = new Room(4060,50,34,40);
-		Room b = new Room(3052,50,40,40);
-		Room c = new Room(2042,40,60,60);
-		Room d = new Room(1040,30,45,60);
-		Room e = new Room(3010,75,50,50);
-		Room f = new Room(4012,25,30,60);
-		
-		RoomDB rooms = new RoomDB();
-		rooms.addRoom(a);
-		rooms.addRoom(b);
-		rooms.addRoom(c);
-		rooms.addRoom(d);
-		rooms.addRoom(e);
-		rooms.addRoom(f);
-		
-		//Creating bookableTimes for Room 4060
-		RoomSchedule a_schedule = new RoomSchedule(a);
-		LocalTime a_startTime =  LocalTime.of(6, MINUTE);
-		LocalTime a_endTime = LocalTime.of(23, MINUTE);
-		TimeSpan a_ts = new TimeSpan(a_startTime,a_endTime);
-		DayOfWeekTimeSpan a_M = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, a_ts);
-		DayOfWeekTimeSpan a_T = new DayOfWeekTimeSpan(DayOfWeek.TUESDAY, a_ts);
-		DayOfWeekTimeSpan a_W = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, a_ts);
-		DayOfWeekTimeSpan a_R = new DayOfWeekTimeSpan(DayOfWeek.THURSDAY, a_ts);
-		DayOfWeekTimeSpan a_F = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, a_ts);
-		DayOfWeekTimeSpan a_S = new DayOfWeekTimeSpan(DayOfWeek.SATURDAY, a_ts);
-		DayOfWeekTimeSpan a_Su = new DayOfWeekTimeSpan(DayOfWeek.SUNDAY, a_ts);
-		
-		a_schedule.addBookableTime(a_F);
-		a_schedule.addBookableTime(a_T);
-		a_schedule.addBookableTime(a_W);
-		a_schedule.addBookableTime(a_Su);
-		a_schedule.addBookableTime(a_M);
-		a_schedule.addBookableTime(a_S);
-		a_schedule.addBookableTime(a_R);
-
-		//Creating bookableTimes for Room 3052
-		RoomSchedule b_schedule = new RoomSchedule(b);
-		LocalTime b_startTime =  LocalTime.of(9, MINUTE);
-		LocalTime b_endTime = LocalTime.of(17, MINUTE);
-		TimeSpan b_ts = new TimeSpan(b_startTime,b_endTime);
-		DayOfWeekTimeSpan b_M = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, b_ts);
-		DayOfWeekTimeSpan b_T = new DayOfWeekTimeSpan(DayOfWeek.TUESDAY, b_ts);
-		DayOfWeekTimeSpan b_W = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, b_ts);
-		DayOfWeekTimeSpan b_R = new DayOfWeekTimeSpan(DayOfWeek.THURSDAY, b_ts);
-		DayOfWeekTimeSpan b_F = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, b_ts);
-		DayOfWeekTimeSpan b_S = new DayOfWeekTimeSpan(DayOfWeek.SATURDAY, b_ts);
-		DayOfWeekTimeSpan b_Su = new DayOfWeekTimeSpan(DayOfWeek.SUNDAY, b_ts);
-		
-		b_schedule.addBookableTime(b_M);
-		b_schedule.addBookableTime(b_T);
-		b_schedule.addBookableTime(b_W);
-		b_schedule.addBookableTime(b_R);
-		b_schedule.addBookableTime(b_F);
-		b_schedule.addBookableTime(b_S);
-		b_schedule.addBookableTime(b_Su);
-		
-		//Creating RoomSchedule for Room 2042
-		RoomSchedule c_schedule = new RoomSchedule(c);
-		LocalTime c_startTime =  LocalTime.of(9, MINUTE);
-		LocalTime c_endTime = LocalTime.of(17, MINUTE);
-		TimeSpan c_ts = new TimeSpan(c_startTime,c_endTime);
-		TimeSpan c_weekend = new TimeSpan(LocalTime.of(6, MINUTE),LocalTime.of(18, MINUTE));
-		DayOfWeekTimeSpan c_M = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, c_ts);
-		DayOfWeekTimeSpan c_T = new DayOfWeekTimeSpan(DayOfWeek.TUESDAY, c_ts);
-		DayOfWeekTimeSpan c_W = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, c_ts);
-		DayOfWeekTimeSpan c_R = new DayOfWeekTimeSpan(DayOfWeek.THURSDAY, c_ts);
-		DayOfWeekTimeSpan c_F = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, c_ts);
-		DayOfWeekTimeSpan c_S = new DayOfWeekTimeSpan(DayOfWeek.SATURDAY, c_weekend);
-		DayOfWeekTimeSpan c_Su = new DayOfWeekTimeSpan(DayOfWeek.SUNDAY, c_weekend);
-		
-		c_schedule.addBookableTime(c_M);
-		c_schedule.addBookableTime(c_T);
-		c_schedule.addBookableTime(c_W);
-		c_schedule.addBookableTime(c_R);
-		c_schedule.addBookableTime(c_F);
-		c_schedule.addBookableTime(c_S);
-		c_schedule.addBookableTime(c_Su);
-		
-		//Creating RoomSchedule for Room 1040
-		RoomSchedule d_schedule = new RoomSchedule(d);
-		LocalTime d_startTime =  LocalTime.of(9, MINUTE);
-		LocalTime d_endTime = LocalTime.of(12, MINUTE);
-		TimeSpan d_ts = new TimeSpan(d_startTime,d_endTime);
-		TimeSpan d_weekend= new TimeSpan(LocalTime.of(6, MINUTE),LocalTime.of(18, MINUTE));
-		DayOfWeekTimeSpan d_M = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, d_ts);
-		DayOfWeekTimeSpan d_M1 = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, new TimeSpan(LocalTime.of(13, MINUTE),LocalTime.of(23, MINUTE)));
-		DayOfWeekTimeSpan d_T = new DayOfWeekTimeSpan(DayOfWeek.TUESDAY, d_ts);
-		DayOfWeekTimeSpan d_W = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, d_ts);
-		DayOfWeekTimeSpan d_W1 = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, new TimeSpan(LocalTime.of(13, MINUTE),LocalTime.of(23, MINUTE)));
-		DayOfWeekTimeSpan d_R = new DayOfWeekTimeSpan(DayOfWeek.THURSDAY, d_ts);
-		DayOfWeekTimeSpan d_F = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, d_ts);
-		DayOfWeekTimeSpan d_F1 = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, new TimeSpan(LocalTime.of(13, MINUTE),LocalTime.of(23, MINUTE)));
-		DayOfWeekTimeSpan d_S = new DayOfWeekTimeSpan(DayOfWeek.SATURDAY, d_weekend);
-		DayOfWeekTimeSpan d_Su = new DayOfWeekTimeSpan(DayOfWeek.SUNDAY, d_weekend);
-		
-		d_schedule.addBookableTime(d_M1);
-		d_schedule.addBookableTime(d_T);
-		d_schedule.addBookableTime(d_M);
-		d_schedule.addBookableTime(d_W);
-		d_schedule.addBookableTime(d_W1);
-		d_schedule.addBookableTime(d_R);
-		d_schedule.addBookableTime(d_S);
-		d_schedule.addBookableTime(d_F1);
-		d_schedule.addBookableTime(d_F);
-		d_schedule.addBookableTime(d_Su);
-	
-		//Creating RoomSchedule for Room 3010
-		RoomSchedule e_schedule = new RoomSchedule(e);
-		LocalTime e_startTime =  LocalTime.of(9, MINUTE);
-		LocalTime e_endTime = LocalTime.of(23, MINUTE);
-		TimeSpan e_ts = new TimeSpan(e_startTime,e_endTime);
-		TimeSpan e_weekend = new TimeSpan(LocalTime.of(6, MINUTE),LocalTime.of(18, MINUTE));
-		DayOfWeekTimeSpan e_M = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, e_ts);
-		DayOfWeekTimeSpan e_T = new DayOfWeekTimeSpan(DayOfWeek.TUESDAY, e_ts);
-		DayOfWeekTimeSpan e_W = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, e_ts);
-		DayOfWeekTimeSpan e_R = new DayOfWeekTimeSpan(DayOfWeek.THURSDAY, e_ts);
-		DayOfWeekTimeSpan e_F = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, e_ts);
-		DayOfWeekTimeSpan e_S = new DayOfWeekTimeSpan(DayOfWeek.SATURDAY, e_weekend);
-		DayOfWeekTimeSpan e_Su = new DayOfWeekTimeSpan(DayOfWeek.SUNDAY, e_weekend);
-		
-		e_schedule.addBookableTime(e_M);
-		e_schedule.addBookableTime(e_T);
-		e_schedule.addBookableTime(e_W);
-		e_schedule.addBookableTime(e_R);
-		e_schedule.addBookableTime(e_F);
-		e_schedule.addBookableTime(e_S);
-		e_schedule.addBookableTime(e_Su);
-		
-		//Creating RoomSchedule for Room 4012
-		RoomSchedule f_schedule = new RoomSchedule(f);
-		LocalTime f_startTime =  LocalTime.of(9, MINUTE);
-		LocalTime f_endTime = LocalTime.of(17, MINUTE);
-		TimeSpan f_ts = new TimeSpan(f_startTime,f_endTime);
-		TimeSpan f_weekend = new TimeSpan(LocalTime.of(6, MINUTE),LocalTime.of(18, MINUTE));
-		DayOfWeekTimeSpan f_M = new DayOfWeekTimeSpan(DayOfWeek.MONDAY, f_ts);
-		DayOfWeekTimeSpan f_T = new DayOfWeekTimeSpan(DayOfWeek.TUESDAY, f_ts);
-		DayOfWeekTimeSpan f_W = new DayOfWeekTimeSpan(DayOfWeek.WEDNESDAY, f_ts);
-		DayOfWeekTimeSpan f_R = new DayOfWeekTimeSpan(DayOfWeek.THURSDAY, f_ts);
-		DayOfWeekTimeSpan f_F = new DayOfWeekTimeSpan(DayOfWeek.FRIDAY, f_ts);
-		DayOfWeekTimeSpan f_S = new DayOfWeekTimeSpan(DayOfWeek.SATURDAY, f_weekend);
-		DayOfWeekTimeSpan f_Su = new DayOfWeekTimeSpan(DayOfWeek.SUNDAY, f_weekend);
-		
-		f_schedule.addBookableTime(f_M);
-		f_schedule.addBookableTime(f_T);
-		f_schedule.addBookableTime(f_W);
-		f_schedule.addBookableTime(f_R);
-		f_schedule.addBookableTime(f_F);
-		f_schedule.addBookableTime(f_S);
-		f_schedule.addBookableTime(f_Su);
-		
-		//Adding roomSchedules to Calendar
-		cal = new Calendar();
-		cal.addRoomSchedule(a_schedule);
-		cal.addRoomSchedule(b_schedule);
-		cal.addRoomSchedule(c_schedule);
-		cal.addRoomSchedule(d_schedule);
-		cal.addRoomSchedule(e_schedule);
-		cal.addRoomSchedule(f_schedule);
-		
-		//LocalDateTimes for a request
-		LocalDateTime as = LocalDateTime.of(2017, 11, 22, 10, MINUTE);
-		LocalDateTime ae = LocalDateTime.of(2017, 11, 22, 12, MINUTE);	
-		
-		
-		Request ar = new Request("Eric Roy", "Elli", 7097463956L, "eelli@mun.ca",1040,as.plusHours(3),ae.plusHours(3),"Testing Request");
-		Request ar1 = new Request("Eric Roy", "Elli",7097463956L,"eelli@mun.ca",1040,as,ae,"Testing Request");
-		
-		cal.getRoomSchedule(1040).addBooking(ar);
-		cal.getRoomSchedule(1040).addBooking(ar1);
+	//deserialized Calendar
+	public void initializeCalendar() {
+		try {
+			FileInputStream fis = new FileInputStream("docs/fullSchedule.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			fullCalendar = (Full_Calendar) ois.readObject();
+			ois.close();
+			fis.close();
+		}catch(IOException i) {
+			i.printStackTrace();
+			return;
+		}catch(ClassNotFoundException e) {
+			System.out.println("Full_Calendar not found!");
+			e.printStackTrace();
+			return;
+		}
 	}
-	
+
 	public JPanel getPanel() {
 		return panel;
 	}
